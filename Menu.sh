e47cc19acbac7e6c257283c374f04989bac53621
enveraslan <enveraslan@marun.edu.tr>
2020-11-19 23:33:50 +0300

    five is left

--- Menu.sh
+++ Menu.sh
@@ -4,24 +4,43 @@
  #
  while :
   do
-     clear
-     echo -e "Please select an option: \n"
+
+     echo "-------------------------------------"
+     echo "             Main Menu "
+     echo "-------------------------------------"
      echo "   1. Create histogram"
      echo "   2. Encryption"
      echo "   3. Delete oldest"
      echo "   4. Convert numbers"
      echo "   5. Organized files"
-     echo -e "   6. Exit\n"
+     echo "   6. Exit\n"
+     echo -n "Enter your menu choice [1-6]: "
 
      read yourch
+     echo -e "\n"
      case $yourch in
-        1) echo "choice-1" ; echo ". . ."; read ;;
- 	    2) echo "choice-2" ; echo ". . ."; read ;;
- 	    3) echo "choice-3" ; echo ". . ."; read ;;
- 	    4) echo "choice-4" ; echo ". . ."; read ;;
- 	    5) echo "choice-5" ; echo ". . ."; read ;;
-        6) exit 0 ;;
- 	    *) echo "Out of range!!";
- 	   echo "Press a key. . ." ; read ;;
+      1) echo -n "Please the file name that includes number data: "
+         read fileName
+         echo -e "\n"
+         ./myprog1.sh $fileName
+         echo -e "\n";;
+ 	   2) echo -n "Please enter string and encryption number: "
+         read str 
+         ./myprog2.sh `echo $str | cut -d' ' -f 1` `echo $str | cut -d' ' -f 2`
+         echo -e "\n";;
+      3) echo -n "Please enter optional path name (if it exists): "
+         read pathName
+         ./myprog3.sh $pathName;;
+      4) echo -n "Please enter the file name: "
+         read fileName
+         ./myprog4.sh $fileName;;
+      5) echo -n "Please include arguments [like -R \"c*.txt\"clear (R is optional)]: "
+         read str
+         arg1=`echo $str | cut -d' ' -f 1`
+         arg2=`echo $str | cut -d' ' -f 2`
+
+         ./myprog5.sh $arg1 "$arg2";;
+      6) exit 0 ;;
+      *) echo "Out of range!!";;
      esac
   done
